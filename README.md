60aa2f30799fe99ad23afb9ca43d9a05fecb5cd0
Dave Eddy <dave@daveeddy.com>
2015-08-17 14:51:13 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,106 @@
+dhtd
+====
+
+Daemon to manage reading DHT temperature sensors on an interval
+
+Summary
+-------
+
+The DHT sensors for reading temperature data on the Raspberry Pi (Linux)
+require precise timing that the kernel can't often guarantee.  As such,
+the Adafruit Python DHT Library has baked into it a lot of retry and backoff
+logic.
+
+To minimize the chance of losing the race with multiple instances of the
+DHT sensor gathering scripts running simultaneously, this daemon aims to be
+the single source of truth for the DHT sensor.
+
+This daemon polls the temperature and humidity data on an interval (30 seconds
+by default), and exposes the information over an HTTP interface.
+
+Example
+-------
+
+Create a simple config
+
+``` json
+{
+  "web": {
+    "host": "127.0.0.1",
+    "port": 10333
+  },
+  "dht": {
+    "type": 22,
+    "pin": 17
+  },
+  "interval": 30,
+  "sudo": true
+}
+```
+
+- `web` configures how the HTTP server is setup
+- `dht` tells this program what kind of DHT sensor you are polling (11, 22, or 2302) and what gpio pin it is on
+- `interval` is time in seconds to sleep between readings
+- `sudo` whether to call the python script with `sudo`, defaults to true
+
+And start the program with the config
+
+    $ dhtd config.json
+    2015-08-17T19:08:47.496Z calling: sudo /home/dave/dev/dhtd/_dht.py 22 17
+    2015-08-17T19:08:57.428Z success: {"farenheit": 77.71999931335449, "celsius": 25.399999618530273, "humidity": 45.599998474121094}
+    2015-08-17T19:08:57.583Z server started: http://127.0.0.1:10333
+    2015-08-17T19:08:58.924Z 127.0.0.1 GET /data
+    2015-08-17T19:09:04.294Z 127.0.0.1 GET /data.json
+
+Human readable data is available at `GET /data`
+
+    $ curl -sS localhost:10333/data
+    farenheit: 77.72F
+    celsius: 25.40C
+    humidity: 45.60%
+    reading: 2015-08-17T19:08:57.421Z
+
+And json data at `GET /data.json`
+
+    $ curl -sS localhost:10333/data.json | json
+    {
+      "farenheit": 77.71999931335449,
+      "celsius": 25.399999618530273,
+      "humidity": 45.599998474121094,
+      "reading": "2015-08-17T19:08:57.421Z"
+    }
+
+Notes
+-----
+
+The daemon will wait until an initial reading of the sensor is taking before
+bringing up the HTTP server.  Afterwards, any failures will be recorded in
+the logs (stdout), but requests to `/data` and `/data.json` will have the
+last previous successful reading data.  This is why the `reading` property
+exists to see when the last successful reading was.
+
+Installation
+------------
+
+### Adafruit Python DHT Library
+
+This program depends on the [Adafruit Python DHT Library](https://github.com/adafruit/Adafruit_Python_DHT)
+in order to work properly - you can install it with:
+
+    sudo apt-get update
+    sudo apt-get install -y build-essential python-dev
+    mkdir -p ~/dev
+    git clone git://github.com/adafruit/Adafruit_Python_DHT ~/dev/Adafruit_Python_DHT
+    cd ~/dev/Adafruit_Python_DHT
+    sudo python setup.py install
+
+### Node.js
+
+Node version 8 or higher
+
+    npm install -g dhtd
+
+License
+-------
+
+MIT License
---
+++ dhtd.js
@@ -0,0 +1,130 @@
+#!/usr/bin/env node
+/**
+ * DHT Daemon
+ *
+ * a daemon for reading temperature as recorded by a DHT
+ * device periodically and exposing the information over HTTP
+ * as JSON.
+ *
+ * Author: Dave Eddy <dave@daveeddy.com>
+ * Date: August 17, 2015
+ * License: MIT
+ */
+
+var execFile = require('child_process').execFile;
+var fs = require('fs');
+var http = require('http');
+var path = require('path');
+var util = require('util');
+
+function log() {
+  var s = util.format.apply(util, arguments);
+  console.log('%s %s', (new Date().toISOString()), s);
+}
+
+var DHT_PROGRAM = path.join(__dirname, '_dht.py');
+
+// config passed as first arg
+var configfile = process.argv[2];
+if (!configfile) {
+  console.error('config file must be specified as the first argument');
+  process.exit(1);
+}
+var config = JSON.parse(fs.readFileSync(configfile, 'utf8'));
+if (!(config.dht && config.dht.pin && config.dht.type)) {
+  console.error('dht data not found in config');
+  process.exit(1);
+}
+
+// normalize config
+config.web = config.web || {};
+config.web.host = config.web.host || '127.0.0.1';
+config.web.port = config.web.port || 10333;
+
+config.interval = (config.interval || 30) * 1000;
+
+var DHT_DATA = {};
+/**
+ * get temperature data from the DHT sensor using the bundled
+ * python script.
+ *
+ * this is called in an interval (actually a setTimeout loop)
+ */
+function gettemps(cb) {
+  var options = {encoding: 'utf8'};
+  var cmd = ['sudo', DHT_PROGRAM, config.dht.type, config.dht.pin];
+  if (config.sudo === false)
+    cmd = cmd.slice(1);
+
+  log('calling: %s', cmd.join(' '));
+  execFile(cmd[0], cmd.slice(1), options, function(err, stdout, stderr) {
+    if (err) {
+      log('failed to call dht.py');
+      console.warn(err);
+      cb(err);
+      return;
+    }
+
+    var o;
+    try {
+      o = JSON.parse(stdout);
+    } catch(e) {
+      log('failed to parse stdout from DHT');
+      console.warn(e);
+      console.dir(stdout);
+      cb(e);
+      return;
+    }
+    o.reading = new Date().toISOString();
+
+    // succesful reading
+    log('success: %s', stdout.trim());
+    DHT_DATA.json = JSON.stringify(o);
+    DHT_DATA.human = [
+      util.format('farenheit: %sF', o.farenheit.toFixed(2)),
+      util.format('celsius: %sC', o.celsius.toFixed(2)),
+      util.format('humidity: %s%%', o.humidity.toFixed(2)),
+      util.format('reading: %s', o.reading)
+    ].join('\n') + '\n';
+    cb();
+  });
+}
+
+/**
+ * a simple wrapper to loop gettemps()
+ */
+function loopgettemps() {
+  setTimeout(function() {
+    gettemps(loopgettemps);
+  }, config.interval);
+}
+
+// get an initial temperature reading before starting the daemon... this will crashe
+// the process if the initial reading fails.
+gettemps(function(err) {
+  if (err)
+    throw err;
+
+  // start HTTP server
+  http.createServer(onrequest).listen(config.web.port, config.web.host, started);
+
+  // start the loop
+  loopgettemps();
+});
+
+function started() {
+  log('server started: http://%s:%d', config.web.host, config.web.port);
+}
+
+function onrequest(req, res) {
+  log('%s %s %s', req.connection.remoteAddress, req.method, req.url);
+  switch (req.url) {
+    case '/ping': res.end('pong\n'); break;
+    case '/data': res.end(DHT_DATA.human); break;
+    case '/data.json': res.end(DHT_DATA.json); break;
+    default:
+      res.statusCode = 404;
+      res.end();
+      break;
+  }
+}
